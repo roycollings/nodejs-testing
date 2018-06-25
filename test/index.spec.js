64ac7360e73a69811a9a1d9824a2e1899989eb8b
Roy Collings <roy.collings@gmail.com>
2018-06-26 00:59:50 +0100

    Split more evenly for demo

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,8 @@
-'use strict'
-
-const assert = require('assert')
-const adder = require('../src/adder')
+const assert = require('assert');
+var basicMaths = require('../src/BasicMaths');
 
 describe('Test', () => {
   it('should add 2 and return the total', () => {
-    assert (4 === adder(2));
+    assert(4 === basicMaths.add(2, 2));
   })
 })
