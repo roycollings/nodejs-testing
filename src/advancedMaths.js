d4891853b12782278334da5d6f116d09a24f4c0a
Roy Collings <roy.collings@gmail.com>
2018-06-26 01:57:15 +0100

    Corrected module

--- src/advancedMaths.js
+++ src/advancedMaths.js
@@ -1,9 +1,9 @@
 module.exports = {
-    multiply = (value1, value2) => {
+    "multiply": (value1, value2) => {
         return value1 * value2;
     },
 
-    divide = (value1, value2) => {
+    "divide": (value1, value2) => {
         return value1 / value2;
     }
 }
