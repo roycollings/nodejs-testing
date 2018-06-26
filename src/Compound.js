9aeb8a177f68114dcb8eed35b24132ecd67bd7c1
Roy Collings <roy.collings@gmail.com>
2018-06-26 01:08:42 +0100

    Added compound functions

---
+++ src/Compound.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "add": (value1, value2, value3) => {
+        return value1 + value2 + value3;
+    },
+
+    "subtract": (value1, value2, value3) => {
+        return value1 - value2 - value3;
+    }
+}
