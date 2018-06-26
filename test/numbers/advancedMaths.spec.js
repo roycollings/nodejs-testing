3edfb75ddd9c409a4f5566267e970362ba50edaf
Roy Collings <roy.collings@gmail.com>
2018-06-26 12:20:41 +0100

    wording change in test

--- test/numbers/advancedMaths.spec.js
+++ test/numbers/advancedMaths.spec.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 var advancedMaths = require('../../src/numbers/advancedMaths.js');
 
-describe('Advanced Maths', () => {
+describe('advancedMaths.multiply', () => {
   it('should multiply the two numbers and return the total', () => {
     assert(4 === advancedMaths.multiply(2, 2));
   })
 })
 
-describe('Advanced Maths', () => {
+describe('advancedMaths.divide', () => {
   it('should divide the first number by the second and return the total', () => {
     assert(5 === advancedMaths.divide(10, 2));
   })
